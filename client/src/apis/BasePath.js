5218c010c477647c34366dc47409ccbed2747e09
ajinkyaap <ajinkyapatankar3121@gmail.com>
2024-02-02 12:33:49 +0530

    AWS ec2 deployment

--- client/src/apis/BasePath.js
+++ client/src/apis/BasePath.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:3001/api/v1",
-});
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api/v1"
+    : "http://localhost:3001/api/v1";
+
+    export default axios.create({
+      baseURL,
+    });
